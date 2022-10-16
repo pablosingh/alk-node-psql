ba4058f403a57bbad4807f8cd92987457cbfc41d
Singh Pablo <pablo22_4@hotmail.com>
2022-10-15 22:28:14 -0300

    add cliente

--- src/models/Person.js
+++ src/models/Person.js
@@ -3,19 +3,29 @@ const { DataTypes } = require('sequelize');
 const { db } = require('../db');
 
 const Person = db.define('person', {
-    id:{
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        primaryKey: true,
-        autoIncrement: true
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+        },
+        email: {
+            type: DataTypes.STRING,
+        },
+        password: {
+            type: DataTypes.STRING,
+        },
+        balance: {
+            type: DataTypes.FLOAT,
+        }
     },
-    name: {
-        type: DataTypes.STRING,
-    },
-    balance: {
-        type: DataTypes.FLOAT,
-    },
-});
+    {
+      timestamps: false
+    }
+);
 
 Person.hasMany(Operation, {
     foreinkey: "personId",
--- src/models/operation.js
+++ src/models/operation.js
@@ -2,23 +2,26 @@ const { DataTypes } = require('sequelize');
 const { db } = require('../db');
 
 const Operation = db.define('operation', {
-    id:{
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+      id:{
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+      },
+      type:{
+        type: DataTypes.STRING,
+      },
+      concept: {
+        type: DataTypes.STRING
+      },
+      amount: {
+          type: DataTypes.FLOAT,
+      },
+      dateOp:{
+        type: DataTypes.STRING,
+      },
     },
-    type:{
-      type: DataTypes.STRING,
-    },
-    concept: {
-      type: DataTypes.STRING
-    },
-    amount: {
-        type: DataTypes.FLOAT,
-    },
-    dateOp:{
-      type: DataTypes.STRING,
-    },
-  });
+    {
+      timestamps: false
+    });
 
 module.exports = Operation;
